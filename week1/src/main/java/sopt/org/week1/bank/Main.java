707161bea75dcb87ba03cbade313f314857cbebc
sojungpp <sophie2458@naver.com>
2023-04-20 01:38:17 +0900

    #1 feat: 유저 로그인 기능 추가

--- week1/src/main/java/sopt/org/week1/bank/Main.java
+++ week1/src/main/java/sopt/org/week1/bank/Main.java
@@ -5,35 +5,63 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-@SpringBootApplication
 public class Main {
 
     static Scanner scanner;
     public static ArrayList<User> userList;
     public static ArrayList<Account> aAccountList;
     public static ArrayList<SavingAccount> sAccountList;
+    private static User user;
 
     public static void main(String[] args) {
+        scanner = new Scanner(System.in);
         userList = new ArrayList<>();
         aAccountList = new ArrayList<>();
         sAccountList = new ArrayList<>();
+        user = new User();
         run();
     }
 
     private static void run() {
-        Exit:
         while (true) {
             System.out.println("------------------- 🏦 솝트 은행 🏦 -------------------");
             System.out.println("1. 로그인 | 2. 회원 가입 | 0. 종료");
             System.out.print("번호를 입력해주세요: ");
             switch (scanner.nextInt()) {
                 case 1:
+                    login();
                     break;
                 case 2:
                     break;
                 case 0:
-                    break Exit;
+                    System.exit(0);
+                default:
+                    System.out.println("** 없는 메뉴입니다. **");
+                    run();
+                    break;
             }
         }
     }
+
+    private static void login() {
+        System.out.println("------------------- 로그인 -------------------");
+        System.out.print("주민등록번호: ");
+        String ssn = scanner.next();
+        System.out.print("비밀번호: ");
+        String password = scanner.next();
+
+        if(!user.getUser(ssn)) {
+            System.out.println("** 존재하지 않는 유저입니다. **");
+            run();
+        } else if(!user.login(ssn, password)) {
+            System.out.println("** 비밀번호가 맞지 않습니다. **");
+            run();
+        } else showMenu();
+    }
+
+    private static void showMenu() {
+        System.out.println("1. 예금 | 2. 출금 | 3. 계좌 조회 | 0. 로그아웃");
+        System.out.print("번호를 입력해주세요: ");
+    }
+
 }
--- week1/src/main/java/sopt/org/week1/bank/User.java
+++ week1/src/main/java/sopt/org/week1/bank/User.java
@@ -1,9 +1,65 @@
 package sopt.org.week1.bank;
 
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static sopt.org.week1.bank.BaseStatus.INVALID_USER;
+import static sopt.org.week1.bank.Main.userList;
+
 public class User {
     private Long id;
     private String name;
     private String ssn;
     private String password;
 
+    public User() {
+    }
+
+
+
+    public boolean getUser(String ssn) {
+        long isUser = userList.stream()
+                .filter(u -> u.getSsn().equals(ssn)).count();
+        return isUser != 0;
+    }
+
+    public boolean login(String ssn, String password) {
+        User user = userList.stream()
+                .filter(u -> u.getSsn().equals(ssn))
+                .findFirst().orElse(null);
+        if(user.getPassword().equals(password)) return true;
+        return false;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
